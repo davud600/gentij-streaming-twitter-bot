4742ab2b90bb1a56c5e370e385244da0fd2ad288
davud <davudgorqi@gmail.com>
2022-12-26 11:11:16 +0100

    interval time fixed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import fetch from "node-fetch";
 import Twit from "twit";
 
 const username = "gentij";
-const intervalTimeMillisec = 1 * 1000;
+const intervalTimeMillisec = 60 * 1000;
 const tweetStatus =
     "Gentij is now streaming live on twitch, GO WATCH!   https://www.twitch.tv/gentij";
 
